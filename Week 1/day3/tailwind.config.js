6588d82f26c46bf7aa3e45cfdeffefcd38b500ba
Winner Never Quit <noman_mumtaz_intern@netixsol.com>
2025-07-31 21:12:20 +0500

    test inline style image

--- Week 1/day3/tailwind.config.js
+++ Week 1/day3/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: theme => ({
+        'heropattern': "url('./images/1.jpg')"
+    })
+    },
   },
   plugins: [],
 }
